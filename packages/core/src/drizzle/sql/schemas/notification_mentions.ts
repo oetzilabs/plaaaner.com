5770feebd70865868350de1ada60c4f33a60c7e4
Özgür Isbert <oezguerisbert@gmail.com>
2024-03-04 13:20:58 +0100

    fix: added relations for notifications_mention

--- packages/core/src/drizzle/sql/schemas/notification_mentions.ts
+++ packages/core/src/drizzle/sql/schemas/notification_mentions.ts
@@ -1,6 +1,8 @@
 import { timestamp, uuid, primaryKey } from "drizzle-orm/pg-core";
 import { schema } from "./utils";
 import { users } from "./users";
+import { relations } from "drizzle-orm";
+import { notifications } from "./notifications";
 
 export const notification_mentions = schema.table(
   "notification_mentions",
@@ -24,3 +26,14 @@ export const notification_mentions = schema.table(
     };
   }
 );
+
+export const notification_mentions_relations = relations(notification_mentions, ({ one }) => ({
+  user: one(users, {
+    fields: [notification_mentions.userId],
+    references: [users.id],
+  }),
+  notification: one(notifications, {
+    fields: [notification_mentions.notificationId],
+    references: [notifications.id],
+  }),
+}));
